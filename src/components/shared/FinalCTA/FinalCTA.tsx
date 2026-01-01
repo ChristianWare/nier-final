7e041b119c32b01f979c14bf01b60d8e9b1a622b
Chris Ware <c.p.ware@hotmail.com>
2026-01-01 12:57:29 -0700

    Final CTA updated

--- src/components/shared/FinalCTA/FinalCTA.tsx
+++ src/components/shared/FinalCTA/FinalCTA.tsx
@@ -8,39 +8,39 @@ import Img1 from "../../../../public/images/airport2.jpg";
 export default function FinalCTA() {
   return (
     <section className={styles.parent}>
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <LayoutWrapper>
-          <div className={styles.content}>
-            <div className={styles.top}>
-              <SectionHeading text='Contat Us' />
-              <h2 className={`${styles.heading} h1`}>
-                Take the first step toward an elevated travel experience.
-              </h2>
-              <p className={`${styles.copy} h4`}>
-                Schedule your ride today and experience the comfort and
-                reliability of our premium black car service.
-              </p>
-            </div>
-            <div className={styles.bottom}>
-              <div className={styles.btnContainer}>
-                <Button
-                  as='span'
-                  text='Read More'
-                  btnType='underlinedBlack'
-                  arrow
-                />
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <LayoutWrapper>
+            <div className={styles.content}>
+              <div className={styles.top}>
+                <SectionHeading text='Contat Us' />
+                <h2 className={`${styles.heading} h1`}>
+                  Take the first step toward an elevated travel experience.
+                </h2>
+                <p className={`${styles.copy} h4`}>
+                  Schedule your ride today and experience the comfort and
+                  reliability of our premium black car service.
+                </p>
+              </div>
+              <div className={styles.bottom}>
+                <div className={styles.btnContainer}>
+                  <Button
+                    as='span'
+                    text='Contact Us'
+                    btnType='underlinedWhite'
+                    arrow
+                  />
+                </div>
               </div>
             </div>
+          </LayoutWrapper>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.imgContainer}>
+            <Image src={Img1} fill alt='Airport Ride' className={styles.img} />
           </div>
-        </LayoutWrapper>
-      </div>
-      <div className={styles.right}>
-        <div className={styles.imgContainer}>
-          <Image src={Img1} fill alt='Airport Ride' className={styles.img} />
         </div>
       </div>
-    </div>
     </section>
   );
 }
