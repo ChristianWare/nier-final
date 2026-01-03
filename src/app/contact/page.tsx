422e6cc988eb59de43436ca832f463c4382ea9ef
Chris Ware <c.p.ware@hotmail.com>
2026-01-02 19:36:10 -0700

    contact page done

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,6 +2,7 @@ import AboutTestimonials from "@/components/AboutPage/AboutTestimonials/AboutTes
 import ContactPageIntro from "@/components/ContactPage/ContactPageIntro/ContactPageIntro";
 import AboutNumbers from "@/components/shared/AboutNumbers/AboutNumbers";
 import BlogSection from "@/components/shared/BlogSection/BlogSection";
+import FinalCTA from "@/components/shared/FinalCTA/FinalCTA";
 import Nav from "@/components/shared/Nav/Nav";
 
 export default function ContactPage() {
@@ -10,6 +11,7 @@ export default function ContactPage() {
       <Nav background='cream' />
       <ContactPageIntro />
       <AboutTestimonials />
+      <FinalCTA />
       <BlogSection />
       <AboutNumbers />
     </main>
