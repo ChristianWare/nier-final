5d75f6dd960179216958afe588f65ab2e319e479
Chris Ware <c.p.ware@hotmail.com>
2026-02-09 23:59:44 -0700

    build test

--- src/components/HomePage/Hero/Hero.tsx
+++ src/components/HomePage/Hero/Hero.tsx
@@ -66,8 +66,8 @@ export default function Hero() {
               </div>
               <div className={styles.right}>
                 <h1 className={styles.heading}>
-                  Nier Transportation is a black car company serving the Phoenix - 
-                  metro area.
+                  Nier Transportation is a black car company serving the Phoenix
+                  - metro area.
                 </h1>
               </div>
             </div>
@@ -77,4 +77,3 @@ export default function Hero() {
     </section>
   );
 }
-
