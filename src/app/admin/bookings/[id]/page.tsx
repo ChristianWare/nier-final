4188d5d45e7c6bee48198dce125a490a1d962109
Chris Ware <c.p.ware@hotmail.com>
2026-04-12 19:13:00 -0700

    {!isCorporateBooking && (

--- src/app/admin/bookings/[id]/page.tsx
+++ src/app/admin/bookings/[id]/page.tsx
@@ -1516,7 +1516,7 @@ export default async function AdminBookingDetailPage({
                 )}
               </div>
 
-              {!isCorporateBooking && !isGroupBooking && (
+              {!isCorporateBooking && (
                 <>
                   <div className={styles.sectionDivider} />
                   <DepositSetupClient
