e92ed44021446870e929e572db32f41f6caac987
Chris Ware <c.p.ware@hotmail.com>
2026-07-16 15:51:26 -0700

    fix: css module casing for linux builds

--- src/app/admin/invoices/[id]/InvoicePdfPanel.tsx
+++ src/app/admin/invoices/[id]/InvoicePdfPanel.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import toast from "react-hot-toast";
-import styles from "./InvoicePdfpanel.module.css";
+import styles from "./InvoicePdfPanel.module.css";
 
 type Props = {
   invoiceId: string;
